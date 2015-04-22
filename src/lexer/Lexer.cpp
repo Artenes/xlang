c1639a368adcf80ac1268c30e8cd08447795aa0f
Artenes Nogueira <artenesnogueira@dimention.com.br>
2015-04-22 11:11:07 -0300

    Atualizado Lexer.cpp

--- src/lexer/Lexer.cpp
+++ src/lexer/Lexer.cpp
@@ -2,7 +2,7 @@
  * Lexer.cpp
  *
  *  Created on: 15/04/2015
- *      Author: aluno
+ *      Author: Artenes
  */
 
 #include "Lexer.h"
@@ -10,134 +10,158 @@
 
 namespace lexer {
 
-    int Lexer::line = 1;
+int Lexer::line = 1;
 
-    Lexer::Lexer(fstream& file) {
-        this->file = &file;
-        this->peek = ' ';
-    }
+Lexer::Lexer(fstream& file) {
+	this->file = &file;
+	this->peek = ' ';
+}
 
-    int Lexer::getLine() {
-        return line;
-    }
+bool Lexer::isIdentifierStart(char c) {
+	return (isalpha(c) || c == '_');
+}
 
-    char Lexer::nextChar() {
-        peek = file->get();
-        return peek;
-    }
+bool Lexer::isIdentifierPart(char c) {
+	return (isIdentifierStart(c) || isdigit(c));
+}
 
-    bool Lexer::isWhitespace(char c) {
-        switch (c) {
-            case ' ': case '\t': case '\n': return true;
-            default: return false;
-        }
-    }
+void Lexer::reserve(Token* w){
+	keywords.insert(KeyWordsPair(w->getLexeme(), w));
+}
 
-    Token* Lexer::nextToken() {
-        while (isWhitespace(peek)) {
-            if (peek == '\n') line++;
-            nextChar();
-        }
-        switch (peek) {
-                //ASSIGN, EQUAL
-            case '=':
-                nextChar();
-                if (peek == '=') {
-                    nextChar();
-                    return Token::TOKEN_EQUAL;
-                } else
-                    return Token::TOKEN_ASSIGN;
-                //NE, LNOT
-            case '!':
-                nextChar();
-                if (peek == '=') {
-                    nextChar();
-                    return Token::TOKEN_NE;
-                } else
-                    return Token::TOKEN_LNOT;
-                //GT, GE
-            case '>':
-                nextChar();
-                if (peek == '=') {
-                    nextChar();
-                    return Token::TOKEN_GE;
-                } else
-                    return Token::TOKEN_GT;
-                //LT, LE
-            case '<':
-                nextChar();
-                if (peek == '=') {
-                    nextChar();
-                    return Token::TOKEN_LE;
-                } else
-                    return Token::TOKEN_LT;
-                //LAND
-            case '&':
-                nextChar();
-                if (peek == '&') {
-                    nextChar();
-                    return Token::TOKEN_LAND;
-                }
-                //LOR
-            case '|':
-                nextChar();
-                if (peek == '|') {
-                    nextChar();
-                    return Token::TOKEN_LOR;
-                }
-                //MINUS
-            case '-':
-                nextChar();
-                return Token::TOKEN_MINUS;
-                //PLUS
-            case '+':
-                nextChar();
-                return Token::TOKEN_PLUS;
-                //STAR
-            case '*':
-                nextChar();
-                return Token::TOKEN_STAR;
-                //SLASH
-            case '/':
-                nextChar();
-                return Token::TOKEN_SLASH;
-                //LBRACK
-            case '[':
-                nextChar();
-                return Token::TOKEN_LBRACK;
-                //LCURLY
-            case '{':
-                nextChar();
-                return Token::TOKEN_LCURLY;
-                //LPAREN
-            case '(':
-                nextChar();
-                return Token::TOKEN_LPAREN;
-                //RBRACK
-            case ']':
-                nextChar();
-                return Token::TOKEN_RBRACK;
-                //RCURLY
-            case '}':
-                nextChar();
-                return Token::TOKEN_RCURLY;
-                //RPAREN
-            case ')':
-                nextChar();
-                return Token::TOKEN_RPAREN;
-                //SEMI
-            case ';':
-                nextChar();
-                return Token::TOKEN_SEMI;
-            case -1:
-                return Token::TOKEN_EOF;
-        }
-        nextChar();
-        return new Token(Token::UNKNOW);
-    }
+int Lexer::getLine(){
+	return line;
+}
 
-    Lexer::~Lexer() {
+char Lexer::nextChar(){
+	peek = file->get();
+	return peek;
+}
 
-    }
+bool Lexer::isWhitespace(char c){
+	switch (c) {
+	case ' ': case '\t': case '\n': return true;
+	default: return false;
+	}
+}
+
+Token* Lexer::nextToken(){
+	while (isWhitespace(peek)) {
+		if (peek == '\n') line++;
+		nextChar();
+	}
+	switch (peek) {
+	//ASSIGN, EQUAL
+	case '=':
+		nextChar();
+		if (peek == '=') {
+			nextChar();
+			return Token::TOKEN_EQUAL;
+		} else
+			return Token::TOKEN_ASSIGN;
+		//NE, LNOT
+	case '!':
+		nextChar();
+		if (peek == '=') {
+			nextChar();
+			return Token::TOKEN_NE;
+		} else
+			return Token::TOKEN_LNOT;
+		//GT, GE
+	case '>':
+		nextChar();
+		if (peek == '=') {
+			nextChar();
+			return Token::TOKEN_GE;
+		} else
+			return Token::TOKEN_GT;
+		//LT, LE
+	case '<':
+		nextChar();
+		if (peek == '=') {
+			nextChar();
+			return Token::TOKEN_LE;
+		} else
+			return Token::TOKEN_LT;
+		//LAND
+	case '&':
+		nextChar();
+		if (peek == '&') {
+			nextChar();
+			return Token::TOKEN_LAND;
+		}
+		//LOR
+	case '|':
+		nextChar();
+		if (peek == '|') {
+			nextChar();
+			return Token::TOKEN_LOR;
+		}
+		//MINUS
+	case '-':
+		nextChar();
+		return Token::TOKEN_MINUS;
+		//PLUS
+	case '+':
+		nextChar();
+		return Token::TOKEN_PLUS;
+		//STAR
+	case '*':
+		nextChar();
+		return Token::TOKEN_STAR;
+		//SLASH
+	case '/':
+		nextChar();
+		return Token::TOKEN_SLASH;
+		//LBRACK
+	case '[':
+		nextChar();
+		return Token::TOKEN_LBRACK;
+		//LCURLY
+	case '{':
+		nextChar();
+		return Token::TOKEN_LCURLY;
+		//LPAREN
+	case '(':
+		nextChar();
+		return Token::TOKEN_LPAREN;
+		//RBRACK
+	case ']':
+		nextChar();
+		return Token::TOKEN_RBRACK;
+		//RCURLY
+	case '}':
+		nextChar();
+		return Token::TOKEN_RCURLY;
+		//RPAREN
+	case ')':
+		nextChar();
+		return Token::TOKEN_RPAREN;
+		//SEMI
+	case ';':
+		nextChar();
+		return Token::TOKEN_SEMI;
+	case -1:
+		return Token::TOKEN_EOF;
+	default:
+		if (isIdentifierStart(peek)) {
+			stringstream ss;
+			do {
+				ss << peek;
+				nextChar();
+			} while (isIdentifierPart(peek));
+			string s = ss.str();
+			return new Token(Token::IDENTIFIER, s);
+		}
+	}
+	nextChar();
+	return new Token(Token::UNKNOW);
+}
+
+
+
+Lexer::~Lexer() {
+
+}
 
 } /* namespace lexer */
