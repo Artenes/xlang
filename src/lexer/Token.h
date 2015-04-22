5657c500b741a4f1998b0eab374caaf146facc98
Artenes Nogueira <artenesnogueira@dimention.com.br>
2015-04-22 11:11:47 -0300

    Atualizado Token.h

--- src/lexer/Token.h
+++ src/lexer/Token.h
@@ -2,7 +2,7 @@
  * Token.h
  *
  *  Created on: 15/04/2015
- *      Author: aluno
+ *      Author: Artenes
  */
 
 #ifndef TOKEN_H_
@@ -19,46 +19,50 @@ namespace lexer {
 class Token {
 public:
 	enum Tag{
+		//Reserved words
+		IF,
 		//Operators
 		ASSIGN, EQUAL, NE,
-                //Comparsion
-                GT, GE, LT, LE,
-                //Logical
-                LNOT, LAND, LOR,
-                //Arithmetic
-                MINUS, PLUS, STAR, SLASH,
-                //Others
+		//Comparsion
+		GT, GE, LT, LE,
+		//Logical
+		LNOT, LAND, LOR,
+		//Arithmetic
+		MINUS, PLUS, STAR, SLASH,
+		//Others
 		TEOF, UNKNOW, LBRACK, LCURLY, LPAREN,
-                RBRACK, RCURLY, RPAREN, SEMI
+		RBRACK, RCURLY, RPAREN, SEMI, IDENTIFIER
 	};
 
+	//Reserved words
+	static Token *TOKEN_IF;
 	//Operators
-        static Token *TOKEN_ASSIGN;
+	static Token *TOKEN_ASSIGN;
 	static Token *TOKEN_EQUAL;
 	static Token *TOKEN_NE;
-        //Comparsion
-        static Token *TOKEN_GT;
-        static Token *TOKEN_GE;
-        static Token *TOKEN_LT;
-        static Token *TOKEN_LE;
-        //Logical
-        static Token *TOKEN_LNOT;
-        static Token *TOKEN_LAND;
-        static Token *TOKEN_LOR;
-        //Arithmetic
-        static Token *TOKEN_MINUS;
-        static Token *TOKEN_PLUS;
-        static Token *TOKEN_STAR;
-        static Token *TOKEN_SLASH;
+	//Comparsion
+	static Token *TOKEN_GT;
+	static Token *TOKEN_GE;
+	static Token *TOKEN_LT;
+	static Token *TOKEN_LE;
+	//Logical
+	static Token *TOKEN_LNOT;
+	static Token *TOKEN_LAND;
+	static Token *TOKEN_LOR;
+	//Arithmetic
+	static Token *TOKEN_MINUS;
+	static Token *TOKEN_PLUS;
+	static Token *TOKEN_STAR;
+	static Token *TOKEN_SLASH;
 	//Others
 	static Token *TOKEN_EOF;
-        static Token *TOKEN_LBRACK;
-        static Token *TOKEN_LCURLY;
-        static Token *TOKEN_LPAREN;
-        static Token *TOKEN_RBRACK;
-        static Token *TOKEN_RCURLY;
-        static Token *TOKEN_RPAREN;
-        static Token *TOKEN_SEMI;
+	static Token *TOKEN_LBRACK;
+	static Token *TOKEN_LCURLY;
+	static Token *TOKEN_LPAREN;
+	static Token *TOKEN_RBRACK;
+	static Token *TOKEN_RCURLY;
+	static Token *TOKEN_RPAREN;
+	static Token *TOKEN_SEMI;
 
 protected:
 	int tag;
